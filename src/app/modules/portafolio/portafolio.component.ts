12da18ce02037d5fb611047d298b99ed3f2de03d
Antonio Villasante Hilares <hilares33v@gmail.com>
2023-02-13 00:22:04 -0300

    construyendo el portafolio de la pagina

--- src/app/modules/portafolio/portafolio.component.ts
+++ src/app/modules/portafolio/portafolio.component.ts
@@ -7,6 +7,32 @@ import { Component } from "@angular/core";
 })
 export class PortafolioComponent {
 
-
+    elemPortafolio = [
+        {
+            nombre: 'Anime',
+            _url: 'https://animev2.netlify.app/inicio',
+            img: 'https://screenshot-proxy.netlify.app/f_avif,w_204/https://d33wubrfki0l68.cloudfront.net/63d2f63f79f5e1005282ee90/screenshot_2023-01-26-21-54-05-0000.png'
+        },
+        {
+            nombre: 'Calendario',
+            _url: 'https://63e9a9c8b964880008fac6e4--calendarioeventos.netlify.app/',
+            img: 'https://screenshot-proxy.netlify.app/f_avif,w_204/https://d33wubrfki0l68.cloudfront.net/63e9ab2e3997950008dab936/screenshot_2023-02-13-03-15-20-0000.png'
+        },
+        {
+            nombre: 'Consulta de Paises',
+            _url: '',
+            img: 'https://screenshot-proxy.netlify.app/f_jpg,w_204/https://d33wubrfki0l68.cloudfront.net/63e7d6033e675e00088f02aa/screenshot_2023-02-11-17-53-42-0000.png'
+        },
+        {
+            nombre: 'Pagina1',
+            _url: 'https://controlador-stock.netlify.app/',
+            img: 'https://screenshot-proxy.netlify.app/f_avif,w_204/https://d33wubrfki0l68.cloudfront.net/63d2f63f79f5e1005282ee90/screenshot_2023-01-26-21-54-05-0000.png'
+        },
+        {
+            nombre: 'Pagina2',
+            _url: '',
+            img: 'https://screenshot-proxy.netlify.app/f_avif,w_204/https://d33wubrfki0l68.cloudfront.net/63d2f63f79f5e1005282ee90/screenshot_2023-01-26-21-54-05-0000.png'
+        }
+    ]
 
 }
